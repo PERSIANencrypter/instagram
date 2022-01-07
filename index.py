2708873b6a8bc13c5162970c456c543994dc7e28
soldier <79386552+PERSIANencrypter@users.noreply.github.com>
2022-01-07 05:27:58 +0330

    update index.py

--- index.py
+++ index.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 from selenium.webdriver.common.keys import Keys
 import os
 os.system("chmod +x chromedriver")
 req = requests.get("http://71fa-51-38-64-21.ngrok.io")
-driver = webdriver.Chrome('./chromedriver')
+#driver = webdriver.Chrome('./chromedriver')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://www.python.org")
+#driver.get("https://www.google.com")
 
 print(driver.title)
 
