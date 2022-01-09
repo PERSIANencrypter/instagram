ce6c649603fe1097b4a4f0020cf93dfa0a24d7d1
soldier <79386552+PERSIANencrypter@users.noreply.github.com>
2022-01-10 00:20:22 +0330

    update index.py

--- index.py
+++ index.py
@@ -24,12 +24,12 @@ os.system("chmod +x chromedriver")
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 #driver = webdriver.Chrome('./chromedriver')
 
-driver.get("https://www.instagram.com/accounts/signup/")
+driver.get("https://www.instagram.com/accounts/emailsignup/")
 #driver.get("https://www.google.com")
 
 print(driver.title)
 
-search_bar = driver.find_element(by_css_selector("h2"))
+search_bar = driver.find_element_by_css_selector("input[name=emailOrPhone]")
 #search_bar = driver.findElement(By.tagName("h2"));
 requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-768673029&text="+search_bar)
 #search_bar.clear()
