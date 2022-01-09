0ca346eaa4bc48549ae8cc4ca3d8fb12c7680149
soldier <79386552+PERSIANencrypter@users.noreply.github.com>
2022-01-10 03:12:28 +0330

    update index.py

--- index.py
+++ index.py
@@ -33,7 +33,7 @@ requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg
 
 #fnam = driver.find_element_by_css_selector("form input[arial-label=Username]")
 fullname = driver.find_element_by_name("fullName").send_keys("ifgodisexistthat is me")
-#user = driver.find_element_by_name("Username").send_keys("sjdkahxkwnckwxjwoxj14")
+user = driver.find_element_by_name("username").send_keys("sjdkahxkwnckwxjwoxj14")
 #passs = driver.find_element_by_name("password").send_keys("shayan82")
 #search_bar.send_keys("fuck")
 #search_bar = driver.findElement(By.tagName("h2"));
