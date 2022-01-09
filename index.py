049e1fabe204b1eabde4156179aef5a21ddf48c8
soldier <79386552+PERSIANencrypter@users.noreply.github.com>
2022-01-10 00:34:35 +0330

    update index.py

--- index.py
+++ index.py
@@ -12,11 +12,11 @@ options = Options()
 options.add_argument("start-maximized")
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
+#chrome_options.add_argument('--no-sandbox')
+#chrome_options.add_argument('--headless')
+#chrome_options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver=webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+#driver=webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
 from selenium.webdriver.common.keys import Keys
 import os
