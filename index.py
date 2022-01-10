d54dc430de6b33ed25ef994fb508f88cf86626aa
soldier <79386552+PERSIANencrypter@users.noreply.github.com>
2022-01-10 04:49:23 +0330

    update index.py

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import requests
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -35,14 +36,16 @@ requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg
 
 #fnam = driver.find_element_by_css_selector("form input[arial-label=Username]")
 email = driver.find_element_by_name("emailOrPhone").send_keys("gholamisadegh76@gmail.com")
-fullname = driver.find_element_by_name("fullName").send_keys("ifgodisexisttekdke")
-user = driver.find_element_by_name("username").send_keys("vaghtsodjwidjwkdjwidjem")
-passs = driver.find_element_by_name("password").send_keys("shadjdjdjyan82")
+fullname = driver.find_element_by_name("fullName").send_keys("ifgoke")
+user = driver.find_element_by_name("username").send_keys("vaghtsodidjem")
+passs = driver.find_element_by_name("password").send_keys("shjsdkw9128ADGOH@@+")
 #login_elem = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[2]/p/a')
 #login_elem = driver.find_element_by_css_selector("button[type=submit]")
 #login_elem.click()
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Sign up']"))).click()
-
+time.sleep(5)
+Select se = new Select(driver.findElement(By.xpath("//*[@title='Month']")));
+se.selectByIndex(3);
 #search_bar = driver.findElement(By.tagName("h2"));
 #requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-768673029&text="+search_bar)
 #search_bar.clear()
