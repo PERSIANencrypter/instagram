a12551bddf46f62ce1e7d1c837aef0133c645243
soldier <79386552+PERSIANencrypter@users.noreply.github.com>
2022-01-17 07:14:57 +0330

    update index.py

--- index.py
+++ index.py
@@ -15,22 +15,22 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import undetected_chromedriver.v2 as uc
-driver = uc.Chrome()
+#driver = uc.Chrome()
 chatIdForLog = "-520315918"
 
 
 options = Options()
-#options = webdriver.ChromeOptions()
+options = webdriver.ChromeOptions()
 options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 #chrome_options.add_argument('--no-sandbox')
 #chrome_options.add_argument('--headless')
 #chrome_options.add_argument('--disable-gpu')
 options.add_argument("start-maximized")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-options.add_argument("--disable-blink-features=AutomationControlled")
-#driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver=webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), options=options)
+#options.add_experimental_option("excludeSwitches", ["enable-automation"])
+#options.add_experimental_option('useAutomationExtension', False)
+#options.add_argument("--disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#driver=webdriver.Chrome(executable_path = os.environ.get("CHROMEDRIVER_PATH"), )
 time.sleep(7)
 os.system("chmod +x chromedriver")
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
