1d8d3a83622191c3a6a25537f1442e1cf10e0d26
soldier <79386552+PERSIANencrypter@users.noreply.github.com>
2022-01-17 05:47:51 +0330

    update index.py

--- index.py
+++ index.py
@@ -1,14 +1,14 @@
-from selenium import webdriver
-import requests
-from selenium.webdriver.support.ui import Select
+import os
 import time
-from selenium.webdriver.support.ui import WebDriverWait
+import json
+import requests
+import random
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import os
-import random
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
@@ -16,6 +16,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import undetected_chromedriver.v2 as uc
 driver = uc.Chrome()
+chatIdForLog = "-520315918"
 
 
 options = Options()
@@ -35,39 +36,37 @@ os.system("chmod +x chromedriver")
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 #driver = webdriver.Chrome('./chromedriver')
 driver.get("https://www.instagram.com/accounts/emailsignup/")
-#requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+driver.title+"1")
-driver.execute_script("window.open('about:blank', 'ftab');")
-driver.switch_to.window("ftab")
-
-driver.get("https://www.instagram.com/accounts/emailsignup/")
+#requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+driver.title+"1")
 time.sleep(5)
-requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+driver.title+"2")
-#going get email
-driver.execute_script("window.open('about:blank', 'secondtab');")
-driver.switch_to.window("secondtab")
-driver.get('https://mail.tm/en/')
-time.sleep(4)
-WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='accounts-menu']"))).click()
-time.sleep(4)
-emm = driver.find_element_by_css_selector('p[class="dark:text-gray-300 text-gray-900 text-sm font-medium leading-5 cursor-pointer select-all truncate"]')
+requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+driver.title+"2")
 time.sleep(1.5)
-requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+driver.title+"3")
-time.sleep(1.5)
-requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+emm.text+"¢")
+requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+driver.title+"3")
+time.sleep(2)
+#requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+emm.text+"¢")
 #emailMovaghat = driver.find_element_by_id("mail")
-#requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+emailMovaghat+"4")
-#requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+emailMovaghat+"4")
-m = str(emm.text)
-
-driver.switch_to.window("ftab")
-time.sleep(9)
-email = driver.find_element_by_name("emailOrPhone").send_keys(m)
+#requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+emailMovaghat+"4")
+#getting mail
+url = "https://www.developermail.com/api/v1/mailbox"
+fristHeader = {"accept": "application/json"}
+createMail = requests.put(url, headers=fristHeader)
+time.sleep(4)
+struct1 = json.loads(createMail.text)
+emailName = struct1["result"]["name"]
+time.sleep(1.5)
+tokenMail = struct1["result"]["token"]
+time.sleep(1.5)
+requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+"this email: "+emailName+"\nwith this token: "+tokenMail+"\n going to be created")
+time.sleep(1.5)
+emailForSignUp = emailName+"@developermail.com"
+time.sleep(3)
+email = driver.find_element_by_name("emailOrPhone").send_keys(emailForSignUp)
 time.sleep(4)
 fristHalf = "Immortal_Guard"
 for i in range(8):
 	shans = random.randint(0, 9)
 	fristHalf += str(shans)
 
+requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+"this username"+fristHalf+"is going yo be create")
 time.sleep(2)
 fullname = driver.find_element_by_name("fullName").send_keys("Immortal")
 time.sleep(4)
@@ -90,23 +89,29 @@ time.sleep(4)
 selecty.select_by_value('1990')
 time.sleep(4)
 WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Next']"))).click()
-time.sleep(30)
-driver.switch_to.window("secondtab")
-time.sleep(10)
-#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='accounts-menu']"))).click()
-time.sleep(8)
-driver.get('https://mail.tm/en/')
+time.sleep(25)
+#gettin id of email from Instagram
+url2 = "https://www.developermail.com/api/v1/mailbox/"+emailName
+hdd = {
+"accept": "application/json",
+"X-MailboxToken": tokenMail
+}
+reqq = requests.get(url2, headers=hdd)
+jsonForId = json.loads(reqq.text)["result"][0]
+#getting message body
+url3 = "https://www.developermail.com/api/v1/mailbox/"+emailName+"/messages/"+jsonForId
+reqqq = requests.get(url3, headers=hdd)
+matn = reqqq.text
+mm = matn.split("Subject")
+mmm = mm[2].split("code")[0].split(" ")
+#mmm is final code
 
-time.sleep(10)
-driver.find_element_by_css_selector("span[calss='truncate']").click()
-time.sleep(3)
-#WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='accounts-menu']"))).click()
 time.sleep(4)
-emmm = driver.find_element_by_css_selector('p[class="dark:text-gray-300 text-gray-900 text-sm font-medium leading-5 cursor-pointer select-all truncate"]')
+user = driver.find_element_by_name("email_confirmation_code").send_keys(mmm[1])
 time.sleep(4)
-requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+emmm.text)
-requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-520315918&text="+driver.title+"5")
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Next']"))).click()
 time.sleep(4)
+requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id="+chatIdForLog+"&text="+driver.title+"5"+"\n created")
 
 #search_bar = driver.findElement(By.tagName("h2"));
 #requests.get("https://api.telegram.org/bot5006110630:AAHkhAo0f3zHVt2Qkpg9UOUb1cg7aJ51538/sendMessage?chat_id=-768673029&text="+search_bar)
